6427607ccd74529b6ab877049450b3c1b0241294
Jean <jeanfilho@gmail.com>
2018-01-11 17:06:20 +0100

    Filled animation controller, updated skeleton

--- Assets/AnimationController.cs
+++ Assets/AnimationController.cs
@@ -7,12 +7,63 @@ public class AnimationController : MonoBehaviour
 {
     public KinectSensor Sensor;
     public Skeleton Skeleton;
+    public Mesh ModelMesh;
 
-	void Awake () {
-		Sensor = KinectSensor.GetDefault();
-	}
-	
-	void Update () {
-		
-	}
+    void Awake()
+    {
+        Sensor = KinectSensor.GetDefault();
+        LoadModel("DO YU KNO DA WAE!?");
+    }
+
+    void Update()
+    {
+        UpdateBones();
+        UpdateMesh();
+    }
+
+    //Load model from the hard drive, map model bones to our bone structure and create a skeleton
+    private void LoadModel(string path)
+    {
+        var basePoseVertices = new Vector3[0];
+        var nameBoneDictionary = new Dictionary<string, BaseBone>();
+        var vertexIdBoneWeightDictionary = new Dictionary<int, Dictionary<string, float>>();
+
+        //TODO: load vertices
+
+        //TODO: load bones - this is just as an example
+        for(int i = 0; i < 0; i++)
+        { 
+            nameBoneDictionary.Add("root", new RootBone(Vector3.zero, Quaternion.identity));
+        }
+
+        //TODO: map vertices to bone id and weight - this is just an example
+        for(int i = 0; i < 0; i++)
+        {
+            var boneWeightDictionary = new Dictionary<string, float>();
+
+            //TODO: Map bone id to weights
+            for(int j = 0; j < 0; j++)
+            {
+                var boneId = "root";
+                boneWeightDictionary.Add(boneId, 1);
+            }
+            vertexIdBoneWeightDictionary.Add(0, boneWeightDictionary);
+        }
+
+        //Create a skeleton
+        Skeleton = new Skeleton(nameBoneDictionary, vertexIdBoneWeightDictionary, basePoseVertices);
+    }
+
+    //Update skeleton using kinect sensor data
+    private void UpdateBones()
+    {
+        //TODO
+    }
+
+    //Apply the bone transformations to the mesh
+    private void UpdateMesh()
+    {
+        //TODO
+        ModelMesh.vertices = Skeleton.GetCurrentPose();
+    }
 }
--- Assets/Skeleton.cs
+++ Assets/Skeleton.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class Skeleton
 {
-    private readonly Dictionary<string, BaseBone> _boneIdBoneDictionary;
-    private readonly Dictionary<int, Dictionary<string, float>> _vertexIdBoneWeightDictionary; 
+    public readonly Dictionary<string, BaseBone> BoneIdBoneDictionary;
+    public readonly Dictionary<int, Dictionary<string, float>> VertexIdBoneWeightDictionary;
+
+    public readonly Vector3[] BasePoseVertices;
 
     //Root bone should have ID == "root"
-    public Skeleton(Dictionary<string, BaseBone> boneIdBoneDictionary, Dictionary<int, Dictionary<string, float>> vertexIdBoneWeightDictionary)
+    public Skeleton(Dictionary<string, BaseBone> boneIdBoneDictionary, Dictionary<int, Dictionary<string, float>> vertexIdBoneWeightDictionary, Vector3[] basePoseVertices)
     {
         //Check if weights are normalized
         foreach (var vertexIdBoneWeight in vertexIdBoneWeightDictionary)
@@ -22,41 +24,42 @@ public class Skeleton
         }
 
         //Check for root bone
-        if(!_boneIdBoneDictionary.ContainsKey("root"))
+        if(!BoneIdBoneDictionary.ContainsKey("root"))
             Debug.LogException(new Exception("No \"root\" bone found in dictionary - Mark the root bone with the id \"root\""));
 
 
         //Assign variables
-        _boneIdBoneDictionary = boneIdBoneDictionary;
-        _vertexIdBoneWeightDictionary = vertexIdBoneWeightDictionary;
+        BoneIdBoneDictionary = boneIdBoneDictionary;
+        VertexIdBoneWeightDictionary = vertexIdBoneWeightDictionary;
+        BasePoseVertices = basePoseVertices;
     }
 
     //Transform mesh vertices accourding to the transformation of each bone
-    public Vector3[] TransformVertices(Vector3[] basePoseVertices)
+    public Vector3[] GetCurrentPose()
     {
         //Deep clone vertices to keep basePoseVertices intact
-        var result = new Vector3[basePoseVertices.Length];
-        Array.Copy(basePoseVertices, result, basePoseVertices.Length);
+        var result = new Vector3[BasePoseVertices.Length];
+        Array.Copy(BasePoseVertices, result, BasePoseVertices.Length);
 
         //Number of bones - required to normalize the weights so that the sum over all weights = 1
-        float numberOfBones = _boneIdBoneDictionary.Count;
+        float numberOfBones = BoneIdBoneDictionary.Count;
 
         //Get the current pose position for every vertex
-        for (int i = 0; i < basePoseVertices.Length; i++)
+        for (int i = 0; i < BasePoseVertices.Length; i++)
         {
             var newPosition = Vector4.zero;
 
             //Sum over all bone influences
-            foreach (var boneWeight in _vertexIdBoneWeightDictionary[i])
+            foreach (var boneWeight in VertexIdBoneWeightDictionary[i])
             {
                 //Value too small to be considered - skip this bone
                 if(Math.Abs(boneWeight.Value) < 0.000001f)
                     continue;
 
-                var currentBone = _boneIdBoneDictionary[boneWeight.Key];
+                var currentBone = BoneIdBoneDictionary[boneWeight.Key];
                 var basePoseMatrix = currentBone.GetLocalBasePoseTransformation();
                 var currentPoseMatrix = currentBone.GetWorldCurrentPoseTransformation();
-                newPosition += currentPoseMatrix * basePoseMatrix * new Vector4(basePoseVertices[i].x, basePoseVertices[i].y, basePoseVertices[i].z, 1);
+                newPosition += currentPoseMatrix * basePoseMatrix * new Vector4(BasePoseVertices[i].x, BasePoseVertices[i].y, BasePoseVertices[i].z, 1);
                 newPosition *= boneWeight.Value;
             }
             //Normalize bone influences
@@ -73,7 +76,13 @@ public abstract class BaseBone
 {
     public Vector3 LocalPosition; //Bone (link) position without any rotation applied to it in local space
     public Quaternion Rotation;
-    public BaseBone Previous;
+
+    protected BaseBone(Vector3 localPosition, Quaternion rotation)
+    {
+        LocalPosition = localPosition;
+        Rotation = rotation;
+    }
+
 
     public abstract Matrix4x4 GetLocalBasePoseTransformation();
     public abstract Matrix4x4 GetWorldCurrentPoseTransformation();
@@ -81,6 +90,8 @@ public abstract class BaseBone
 
 public class RootBone : BaseBone
 {
+    public RootBone(Vector3 localPosition, Quaternion rotation) : base(localPosition, rotation) { }
+
     public override Matrix4x4 GetLocalBasePoseTransformation()
     {
         return Matrix4x4.TRS(Vector3.zero, Rotation, Vector3.one).inverse;
@@ -94,6 +105,13 @@ public class RootBone : BaseBone
 
 public class Bone : BaseBone
 {
+    public BaseBone Previous;
+
+    public Bone(Vector3 localPosition, Quaternion rotation, BaseBone previous) : base(localPosition, rotation)
+    {
+        Previous = previous;
+    }
+
     public override Matrix4x4 GetLocalBasePoseTransformation()
     {
         return Matrix4x4.TRS(LocalPosition, Rotation, Vector3.one).inverse * Previous.GetLocalBasePoseTransformation();
