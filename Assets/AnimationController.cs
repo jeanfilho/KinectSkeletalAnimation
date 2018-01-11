15d70a53a387712391323333e29eb9705879e727
Jean <jeanfilho@gmail.com>
2018-01-11 15:17:06 +0100

    added kinect plugin and some classes

---
+++ Assets/AnimationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Windows.Kinect;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public KinectSensor Sensor;
+
+	void Awake () {
+		Sensor = KinectSensor.GetDefault();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
